05a641e2744d215a53766311c93d7e4ddd940405
yue82 <no.82.vuf.lab@gmail.com>
2025-01-04 19:13:25 +0900

    update local

--- local_main.py
+++ local_main.py
@@ -1,29 +1,36 @@
 # -*- coding: utf-8 -*-
 
 import sys
-import os
 import cv2
+
+import util
 
 
 ## カメラ or 動画 ローカル表示
 def video_movie_local(input_path_or_port, mask_img_path, cascade_file_path):
-    check_file('Mask', mask_img_path)
+    util.check_file('Mask', mask_img_path)
     mark_rgba = cv2.imread(mask_img_path, cv2.IMREAD_UNCHANGED)
     cascade = cv2.CascadeClassifier(cascade_file_path)
 
+    capture = cv2.VideoCapture(input_path_or_port)
+    capture.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
     try:
-        capture = cv2.VideoCapture(input_path_or_port)
-        capture.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
-        while(True):
+        while True:
             ret, target_frame = capture.read()
             target_frame_gray = cv2.cvtColor(target_frame, cv2.COLOR_BGR2GRAY)
             faces = cascade.detectMultiScale(target_frame_gray, scaleFactor=1.1, minNeighbors=3, minSize=(75, 75))
             for face_ps in faces:
-                replace_face(face_ps, target_frame, mark_rgba)
+                util.replace_face(face_ps, target_frame, mark_rgba)
             cv2.imshow("Masked", target_frame)
-            cv2.waitKey(100)
+
+            # 'q' キーが押されたらループを抜ける
+            if cv2.waitKey(1) == ord("q"):
+                break
     except Exception as e:
         print(f'exception: {e}')
+    finally:
+        capture.release()
+        cv2.destroyAllWindows()
 
 
 ## カメラ 接続チェック
@@ -47,8 +54,8 @@ def check_camera_connections(max_camera_num):
 ## 画像 ローカル保存
 def image_local(input_img_path, mask_img_path, cascade_file_path):
 
-    check_file('Input', input_img_path)
-    check_file('Mask', mask_img_path)
+    util.check_file('Input', input_img_path)
+    util.check_file('Mask', mask_img_path)
     mark_rgba = cv2.imread(mask_img_path, cv2.IMREAD_UNCHANGED)
     cascade = cv2.CascadeClassifier(cascade_file_path)
 
@@ -56,34 +63,8 @@ def image_local(input_img_path, mask_img_path, cascade_file_path):
     faces = cascade.detectMultiScale(target_img, scaleFactor=1.1, minNeighbors=3, minSize=(75, 75))
 
     for face_ps in faces:
-        replace_face(face_ps, target_img, mark_rgba)
-    output_img(input_img_path, target_img)
-
-
-def check_file(filetype, img_path):
-    if not os.path.exists(img_path):
-        print(f'No {filetype} File.')
-        exit(1)
-
-
-def replace_face(face_ps, target_img, mark_rgba):
-    # 画像合成用にコピー
-    mark_alpha_tmp = mark_rgba.copy()
-    x, y, w, h = face_ps
-    mark_alpha_tmp = cv2.resize(mark_alpha_tmp, dsize=(w, h))
-
-    # 入力画像の顔の部分にはめ込む
-    for dx in range(0, w):
-        for dy in range(0, h):
-            # アルファチャネルが0でないなら画像を入れ替える
-            if mark_alpha_tmp[dy, dx, 3] != 0:
-                target_img[y + dy, x + dx, :3] = mark_alpha_tmp[dy, dx, :3]
-
-
-def output_img(input_img_path, target_img):
-    output_img_path = 'output_img/{}'.format('/'.join(input_img.split('/')[1:]))
-    print(f'Output: {output_img_path}')
-    cv2.imwrite(output_img_path, target_img)
+        util.replace_face(face_ps, target_img, mark_rgba)
+    util.output_img(input_img_path, target_img)
 
 
 if __name__ == '__main__':
---
+++ util.py
@@ -0,0 +1,27 @@
+import os
+import cv2
+
+def check_file(filetype, img_path):
+    if not os.path.exists(img_path):
+        print(f'No {filetype} File.')
+        exit(1)
+
+
+def replace_face(face_ps, target_img, mark_rgba):
+    # 画像合成用にコピー
+    mark_alpha_tmp = mark_rgba.copy()
+    x, y, w, h = face_ps
+    mark_alpha_tmp = cv2.resize(mark_alpha_tmp, dsize=(w, h))
+
+    # 入力画像の顔の部分にはめ込む
+    for dx in range(0, w):
+        for dy in range(0, h):
+            # アルファチャネルが0でないなら画像を入れ替える
+            if mark_alpha_tmp[dy, dx, 3] != 0:
+                target_img[y + dy, x + dx, :3] = mark_alpha_tmp[dy, dx, :3]
+
+
+def output_img(input_img_path, target_img):
+    output_img_path = 'output_img/{}'.format('/'.join(input_img.split('/')[1:]))
+    print(f'Output: {output_img_path}')
+    cv2.imwrite(output_img_path, target_img)
